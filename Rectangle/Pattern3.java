1d1cb610fd82d63fd1e2bbcfe3ec84a8ca767f97
Rayesmi G J <gjrayesmi37@gmail.com>
2025-08-06 23:11:51 +0530

    Right triangle

---
+++ Rectangle/Pattern3.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Pattern3 {
+    public static void main(String R[]){
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter no of rows:"); 
+     int n=sc.nextInt();
+     for(int i=1;i<=n;i++){
+        for(int j=2;j<=i;j++){
+         System.out.print(" ");
+        }
+        for(int j=1;j<=n;j++){
+            System.out.print("*");
+        }
+        System.err.println();
+     }
+    }
+}
+
+/*
+ OUTPUT:
+ Enter no of rows:
+5
+*****
+ *****
+  *****
+   *****
+    *****
+ */
